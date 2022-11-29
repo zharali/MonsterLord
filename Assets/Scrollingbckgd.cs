55f9668aff14da6c9716b762daca121eb4ec7e9f
zharali <ali.zhara8@gmail.com>
2022-11-29 11:23:58 +0900

    Moving background

---
+++ Assets/Scrollingbckgd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrollingbckgd : MonoBehaviour
+{
+
+    public float bgSpeed;
+    public Renderer bgRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRend.material.mainTextureOffset += new Vector2(0f, bgSpeed * Time.deltaTime) ;
+          
+    }
+}

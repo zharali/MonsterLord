5340af0296ef77e213e636022b5c980bc262052a
luis <luis.martinez.sanchez@gmail.com>
2022-11-29 11:58:53 +0900

    obstacle spawning at the top (still need to merge it with the sky and whole camera moving)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public float playerSpeed;
+	private Rigidbody2D rb;
+	private Vector2 playerDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    	// Not moving vertically, only horizontally (could change in the future?).
+        float directionX = Input.GetAxisRaw("Horizontal");
+        playerDirection = new Vector2(directionX, 0).normalized;
+    }
+    
+    void FixedUpdate()
+    {
+    	rb.velocity = new Vector2(playerDirection.x * playerSpeed, 0);
     }
 }

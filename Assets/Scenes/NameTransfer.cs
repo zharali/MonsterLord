cf5fb993011613b4de72472876d62949f871ee9a
zharali <ali.zhara8@gmail.com>
2022-12-06 13:09:11 +0900

    player scene

---
+++ Assets/Scenes/NameTransfer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NameTransfer : MonoBehaviour
+{
+    public TextMeshProUGUI theName;
+    public TMP_InputField inputField;
+    public TextMeshProUGUI textDisplay;
+
+    public void StoreName()
+    {
+        theName.text = inputField.text;
+        textDisplay.text = "Welcome " + theName + " to MonsterLord!";
+    }
+}

e857963ff5cc49d4c7e38478247f938a842d9094
luis <luis.martinez.sanchez@gmail.com>
2022-12-06 12:16:51 +0900

    obstacles move down dissapear when colliding with border. camera and layers fixed.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+	public float verticalSpeed;
+	public float horizontalSpeed;
+	private Rigidbody2D rb;
+	private Vector2 obstacleDirection;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	float directionX = 0;
+    	float directionY = 1;
+        obstacleDirection = new Vector2(directionX, directionY).normalized;
+    }
+    
+    void FixedUpdate()
+    {
+    	rb.velocity = new Vector2(obstacleDirection.x * horizontalSpeed, obstacleDirection.y * verticalSpeed);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    	if(collision.tag == "Border")
+    	{
+    		Destroy(this.gameObject);    	
+    	}
+    }
+}

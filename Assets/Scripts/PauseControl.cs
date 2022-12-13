c02e629b995c2b87c5208bdece2bfdf10fdf765b
luis <luis.martinez.sanchez@gmail.com>
2022-12-13 13:31:52 +0900

    pause menu. pause works but resume does not yet

---
+++ Assets/Scripts/PauseControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+
+	public static bool gameIsPaused;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        	gameIsPaused = !gameIsPaused; //just toggle the pause boolean.
+        	PauseGame();        
+        }
+    }
+    
+    void PauseGame()
+    {
+    	if(gameIsPaused)
+    	{
+    		Time.timeScale = 0f; 	
+    	}
+    	else
+    	{
+    		Time.timeScale = 1;
+    	}
+    }
+}

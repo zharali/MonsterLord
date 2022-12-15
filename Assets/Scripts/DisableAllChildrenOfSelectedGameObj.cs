4ae54f064384a2f2cd8e8d35332e181fca291209
zharali <ali.zhara8@gmail.com>
2022-12-15 19:29:26 +0900

    character selection menu (display 'issues')

---
+++ Assets/Scripts/DisableAllChildrenOfSelectedGameObj.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableAllChildrenOfSelectedGameObj : MonoBehaviour
+{
+    public GameObject parentGameObject;
+
+    public void DisableAllChildren()
+    {
+        for (int i=0; i < parentGameObject.transform.childCount; i++)
+        {
+            var child = parentGameObject.transform.GetChild(i).gameObject;
+
+            if (child != null)
+            {
+                child.SetActive(false);
+            }
+        }
+    }
+}

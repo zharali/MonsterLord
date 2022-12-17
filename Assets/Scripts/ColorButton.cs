6344d002db069717c67e51a4da26a3084be2a022
zharali <ali.zhara8@gmail.com>
2022-12-17 18:15:39 +0900

    select character color

---
+++ Assets/Scripts/ColorButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorButton : MonoBehaviour
+{
+    public SelectColor selectColor; 
+
+    [Header("Color Values")]
+    public float redAmount;
+    public float greenAmount;
+    public float blueAmount;
+
+    [Header("Color Sliders")]
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+
+    public Image colorImage;
+
+    private void Awake()
+    {
+        colorImage = GetComponent<Image>();
+        redAmount = colorImage.color.r;
+        greenAmount = colorImage.color.g;
+        blueAmount = colorImage.color.b;
+    }
+
+    public void SetSliderValuesToImageColor()
+    {
+        redSlider.value = redAmount;
+        greenSlider.value = greenAmount;
+        blueSlider.value = blueAmount;
+
+        selectColor.redAmount = redAmount;
+        selectColor.greenAmount = greenAmount;
+        selectColor.blueAmount = blueAmount;
+        selectColor.SetColor();
+    }
+}
+
---
+++ Assets/Scripts/SelectColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//to use if we want sliders to select the color 
+public class SelectColor : MonoBehaviour
+{
+    [Header("Color Values")]
+    public float redAmount;
+    public float greenAmount;
+    public float blueAmount;
+
+    [Header("Color Sliders")]
+    public Slider redSlider;
+    public Slider greenSlider;
+    public Slider blueSlider;
+
+    private Color currentColor;
+
+    //grab the material from the renderer and change the color properties of the material
+    public List<Renderer> rendererList = new List<Renderer>();
+
+    //if we want to customize the color, dont need it here 
+    public void UpdateSliders()
+    {
+        redAmount = redSlider.value;
+        greenAmount = greenSlider.value;
+        blueAmount = blueSlider.value;
+        SetColor(); 
+    }
+
+    public void SetColor()
+    {
+        currentColor = new Color(redAmount, greenAmount, blueAmount);
+
+        for (int i=0; i < rendererList.Count; i++)
+        {
+            rendererList[i].material.SetColor("_Color", currentColor);
+        }
+    }
+}

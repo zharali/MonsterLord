07ece9d5fb811565245cdf2d1aac8b13eb861c7a
luis <luis.martinez.sanchez@gmail.com>
2022-12-18 23:31:25 +0900

    added music. in the game scene its lagging for some reason (at least in my laptop)

---
+++ Assets/Scripts/Menu2Game.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu2Game : MonoBehaviour
+{
+    public void Move2Game(int sceneID){
+        SceneManager.LoadScene(sceneID);
+    }
+}

114d11a2218412e68487525eb6ee05835a41f70f
luis <luis.martinez.sanchez@gmail.com>
2022-12-20 00:15:58 +0900

    first attempt at voice controls. kept the arrowkeys for now as i yet have to fix it

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.Windows.Speech;
 
 public class Player : MonoBehaviour
 {
@@ -12,6 +15,7 @@ public class Player : MonoBehaviour
 	public int maxHealth = 100;
 	public long currentHealth;
     public ulong atk;
+    public ScoreManager scoreManager;
     public ulong score;
     public uint gameLevel;
     public uint bossBeaten;
@@ -19,12 +23,20 @@ public class Player : MonoBehaviour
     public HealthBar healthBar;
 	private Rigidbody2D rb;
 	private Vector2 playerDirection;
+	public int sampleWindow = 64;
+	//public AudioSource audioSource;
+	private AudioClip microphoneClip;
+	private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
+	private KeywordRecognizer keywordRecognizer;
 
     #region Singleton
     public static Player instance; 
     // Start is called before the first frame update
     void Start()
     {
+    	
+        //resume time. gameover was messing this up.
+        Time.timeScale = 1f;
         // Singleton shenanigans
         if (instance == null) instance = this;
         else Destroy(this.gameObject);
@@ -37,9 +49,32 @@ public class Player : MonoBehaviour
         gameLevel = 1;
         bossBeaten = 0;
         rb = GetComponent<Rigidbody2D>();
-        Time.timeScale = 1f;
+        
+        //for voice recognition
+        MicrophoneToAudioClip(); //start the microphone
+        keywordActions.Add("hello", MoveLeft); //add words and methods to dictionary
+        keywordActions.Add("turn left", MoveRight);
+        
+        keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognized;
+        keywordRecognizer.Start();        
+        
+        foreach (var device in Microphone.devices)
+    	{
+        	Debug.Log("Name: " + device);
+    	}
+    	/*
+    	var audio = GetComponent< AudioSource > ();
+		audio.clip = Microphone.Start("Default Input Device", true, 10, 44100);
+		audio.loop = true;
+		while (!(Microphone.GetPosition(null) > 0)) { }
+		audio.Play();
+		*/
+		
     }
     #endregion
+    
+    
     // Update is called once per frame
     void Update()
     {
@@ -48,9 +83,58 @@ public class Player : MonoBehaviour
         playerDirection = new Vector2(directionX, 0).normalized;
     }
     
+    public void MicrophoneToAudioClip()
+    {
+    	string microphoneName = Microphone.devices[0];
+    	microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+    
+    public float GetLoudnessFromMicrophone()
+    {
+    	return GetLoudnessFromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+    
+    public float GetLoudnessFromAudioClip(int clipPosition, AudioClip clip)
+    {
+    	int startPosition = clipPosition - sampleWindow;
+    	if (startPosition < 0) return 0; //for the start of the clip error.
+    	float[] waveData = new float[sampleWindow];
+    	clip.GetData(waveData, startPosition);
+    	
+    	//compute loudness
+    	float totalLoudness = 0;
+    	for (int i = 0; i < sampleWindow; i++)
+    	{
+    		totalLoudness += Mathf.Abs(waveData[i]);
+    	}
+    	return totalLoudness / sampleWindow;
+    }
+    
+    private void OnKeywordsRecognized(PhraseRecognizedEventArgs args)
+    {
+    	Debug.Log("Keyword: " + args.text);
+    	keywordActions[args.text].Invoke();
+    }
+    
+    void MoveLeft()
+    {
+    	float loudness = GetLoudnessFromMicrophone();
+    	//Debug.Log(loudness);
+        playerDirection = new Vector2(-1, 0).normalized * loudness;
+    	
+    }
+    
+    void MoveRight()
+    {
+    	float loudness = GetLoudnessFromMicrophone();
+    	//Debug.Log(loudness);
+        playerDirection = new Vector2(1, 0).normalized * loudness;
+    
+    }
+    
     void FixedUpdate()
     {
-    	rb.velocity = new Vector2(playerDirection.x * playerSpeed, 0);
+    	rb.velocity = new Vector2(playerDirection.x * playerSpeed , 0);
     }
     
     private void OnTriggerEnter2D(Collider2D collision)
@@ -75,7 +159,9 @@ public class Player : MonoBehaviour
             ulong currentXp = GlobalData.instance.data.levelxp;
             uint currentLevel = GlobalData.instance.data.characterLevel;
 
-            ulong totalXpEarned = score / 10;  // To be changed or fine-tuned
+			//i think its easier to have score and XP in the ScoreManager script and access it.
+			score = Convert.ToUInt64(scoreManager.GetScore());
+            ulong totalXpEarned = Convert.ToUInt64(scoreManager.GetXp());  // To be changed or fine-tuned
 
             ulong levelUpRequirement = StatFunctions.XpToLevelUp(currentLevel);
 
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ScoreManager : MonoBehaviour
 {
 	public Text scoreText;
 	private float score;
-	public int scoreMultiplier;
+	private float xp;
+	public float scoreMultiplier;
+	public Text finalScoreText;
+	public Text xpText;
 
     // Update is called once per frame
     void Update()
@@ -16,7 +20,19 @@ public class ScoreManager : MonoBehaviour
     	{
     		score += scoreMultiplier * Time.deltaTime;
     		scoreText.text = ((int)score).ToString();    	
+    		finalScoreText.text = "Score: " + ((int)score).ToString();   
+    		xp = score / 10;
+    		xpText.text = "XP: " + ((int)xp).ToString();   
     	}
-        
+    }
+    
+    public float GetScore()
+    {
+    	return score;
+    }
+    
+    public float GetXp()
+    {
+    	return xp;
     }
 }

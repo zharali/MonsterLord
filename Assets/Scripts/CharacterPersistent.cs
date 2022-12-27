16a67178407c537fc69e62850c9e347620a23591
zharali <ali.zhara8@gmail.com>
2022-12-27 12:59:37 +0900

    character persistency (not finished)

---
+++ Assets/Scripts/CharacterPersistent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPersistent : MonoBehaviour
+{
+    public bool keepObjectPersistent = true;
+
+    void Awake()
+    {
+        if (keepObjectPersistent)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}

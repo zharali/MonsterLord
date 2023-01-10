cf054b66dd6c30852be9a7d2f4533b81a8052302
zharali <ali.zhara8@gmail.com>
2023-01-10 15:57:07 +0900

    data

---
+++ Assets/Scripts/CharacterCustomData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class CharacterCustomData : MonoBehaviour
+{
+
+    public GameObject skinColor;
+    public GameObject eyesStyle;
+    public GameObject mouthStyle;
+    public GameObject characterStyle;
+    CharacterCustomizationData customizationData = ScriptableObject.CreateInstance<CharacterCustomizationData>();
+
+
+    public void SetCharacterStyle(GameObject character)
+    {
+        characterStyle = character;
+        customizationData.characterStyle = characterStyle;
+    }
+
+    public GameObject SetSkinColor(GameObject skin)
+    {
+        skinColor = skin;
+        return skin;
+    }
+
+    public GameObject SetEyesStyle(GameObject eyes)
+    {
+        eyesStyle = eyes;
+        return eyes;
+    }
+
+    public GameObject SetMouthStyle(GameObject mouth)
+    {
+        mouthStyle = mouth;
+        return mouth;
+    }
+
+    public void CreateData()
+    {
+       
+        //customizationData.skinColor = skinColor;
+        //customizationData.eyesStyle = eyesStyle;
+        //customizationData.mouthStyle = mouthStyle;
+        AssetDatabase.CreateAsset(customizationData, "Assets/CharacterCustomizationData.asset");
+        AssetDatabase.SaveAssets();
+    }
+    
+}

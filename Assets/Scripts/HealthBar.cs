673f7d4c3d0b2e2d43f3553393e6c4ecbfdb73b1
luis <luis.martinez.sanchez@gmail.com>
2023-01-17 18:45:16 +0900

    added new background. healthbar text. character screen background

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,15 +7,18 @@ public class HealthBar : MonoBehaviour
 {
 
 	public Slider slider;
+	public Text number;
 	
 	public void SetMaxHealth(long health)
 	{
 		slider.maxValue = health;
 		slider.value = health;
+    	number.text = "HP: " + ((int)health).ToString() + "/" + slider.maxValue.ToString();
 	}
     
     public void SetHealth(long health)
     {
     	slider.value = health;
+    	number.text = "HP: " + ((int)health).ToString() + "/" + slider.maxValue.ToString();
     }
 }
